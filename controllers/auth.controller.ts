6996efe22f6885dd23bf5003057118f6741325ed
Souhardya20 <souhardyadeb2016@gmail.com>
2025-07-20 14:03:37 +0530

    test

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -95,7 +95,7 @@ export const login = asyncHandler(async (req: Request, res: Response) => {
 		res.cookie("token", token, {
 			expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
 			httpOnly: true,
-			secure: true,
+			secure: false,
 			sameSite: "none",
 			// domain: ".jgecalumni.in",
 		});
